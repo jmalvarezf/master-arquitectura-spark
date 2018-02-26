9229b86bc741fd0331e93194799c6656c20798ee
ccolado <ccolado@gmail.com>
2018-02-26 09:17:03 +0100

    Sunday session with Chema and Cesar

---
+++ 03-python_streaming.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+import socket, time, csv, urllib, io, os, logging
+print("Started...")
+import urllib.request
+
+url = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-white.csv"
+data = urllib.request.urlopen(url)
+try:
+    os.makedirs("./data")
+except:
+    logging.info("Directory exists, continuing")
+
+remote_file = csv.reader(io.TextIOWrapper(data), delimiter=";")
+i = 0
+for row in remote_file:
+  if i == 0:
+     i+=1
+     continue
+  print(row)
+  f = open("./data/data_"+str(i)+".csv",'w')
+  row.append("wine" + str(i))
+  f.write(";".join(row)) #Give your csv text here.
+  f.close()
+  time.sleep(1.0)
+  i+=1
